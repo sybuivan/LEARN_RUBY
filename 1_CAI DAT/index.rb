5e77b6b56b2332e0c91cf81137d410d326191d36
sybuivan <sy.buivan@ncc.asia>
2022-08-28 20:39:43 +0700

    upload code install ruby and bai 1,2

---
+++ 1_CAI DAT/index.rb
@@ -0,0 +1,15 @@
+puts "Hello. Bui Van Sy. MSV: 1911505310248"
+print "Enter a number: "
+number = gets
+puts "Number is #{number}"
+puts "Enter two number a, b. Compare a, b and print number bigger number
+"
+print "Enter A number: "
+a = gets
+print "Enter B number: "
+b = gets
+if a > b 
+   puts "A is the larger number #{a}"
+else
+   puts "B is the larger number #{b}"
+end
