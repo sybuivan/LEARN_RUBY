3006eada66b2ba9d5fe43e16a0583e91d7266d0f
sybuivan <sy.buivan@ncc.asia>
2022-09-24 16:17:39 +0700

    fix and add image screen result

--- BAITAP_3/Human.rb
+++ BAITAP_3/Human.rb
@@ -1,100 +1,100 @@
 class Human
-   @name;
-   @hair;
-   @age;
-   @height;
-   @weight;
-   @email;
-   @phone;
-   @nation;
+   @name_248;
+   @hair_248;
+   @age_248;
+   @height_248;
+   @weight_248;
+   @email_248;
+   @phone_248;
+   @nation_248;
 
-   def initialize(name, hair, age, height, weight, email, phone,nation)
-      @name = name;
-      @hair = hair;
-      @age = age;
-      @height = height;
-      @weight = weight;
-      @email = email;
-      @phone = phone
-      @nation = nation;
+   def initialize(name_248, hair_248, age_248, height_248, weight_248, email_248, phone_248,nation_248)
+      @name_248 = name_248;
+      @hair_248 = hair_248;
+      @age_248 = age_248;
+      @height_248 = height_248;
+      @weight_248 = weight_248;
+      @email_248 = email_248;
+      @phone_248 = phone_248;
+      @nation_248 = nation_248;
    end
 
-   attr_accessor :name, :hair, :age, :height, :weight, :email, :phone, :nation;
+   attr_accessor :name_248, :hair_248, :age_248, :height_248, :weight_248, :email_248, :phone_248, :nation_248;
 
    def showInformation
-      puts "\nName : #{name}||Hair: #{hair}||Age: #{age}||Height: #{height}||Weight: #{weight}||Email: #{email}||Phone: #{phone}||Nation: #{nation};"
+      puts "\nName : #{name_248}||Hair: #{hair_248}||Age: #{age_248}||Height: #{height_248}||Weight: #{weight_248}||Email: #{email_248}||Phone: #{phone_248}||Nation: #{nation_248};"
     end
 end
 
 class Student < Human
-   $code;
-   $grade_1;
-   $grade_2;
-   $grade_3;
+   $code_248;
+   $grade_1_248;
+   $grade_2_248;
+   $grade_3_248;
 
-   def initialize(name, hair, age, height, weight, email, phone,nation, code, grade_1, grade_2, grade_3)
-      super(name, hair, age, height, weight, email, phone,nation)
-      @code = code;
-      @grade_1 = grade_1;
-      @grade_2 = grade_2;
-      @grade_3 = grade_3;
+   def initialize(name_248, hair_248, age_248, height_248, weight_248, email_248, phone_248,nation_248, code_248, grade_1_248, grade_2_248, grade_3_248)
+      super(name_248, hair_248, age_248, height_248, weight_248, email_248, phone_248,nation_248)
+      @code_248 = code_248;
+      @grade_1_248 = grade_1_248;
+      @grade_2_248 = grade_2_248;
+      @grade_3_248 = grade_3_248;
    end
 
-   def code
-      @code
+   def code_248
+      @code_248
    end
-   def grade_1
-      @grade_1
+   def grade_1_248
+      @grade_1_248
    end
-   def grade_2
-      @grade_2
+   def grade_2_248
+      @grade_2_248
    end
-   def grade_3
-      @grade_3
+   def grade_3_248
+      @grade_3_248
    end
    
-   def code=(code)
-      @code = code
+   def code_248=(code_248)
+      @code_248 = code_248
    end
-   def grade_1=(grade_1)
-      @grade_1 = grade_1
+   def grade_1_248=(grade_1_248)
+      @grade_1_248 = grade_1_248
    end
-   def grade_2=(grade_2)
-      @grade_2 = grade_2
+   def grade_2_248=(grade_2_248)
+      @grade_2_248 = grade_2_248
    end
-   def grade_3=(grade_3)
-      @grade_3 = grade_3
+   def grade_3_248=(grade_3_248)
+      @grade_3_248 = grade_3_248
    end
    
-   def average
-      (@grade_1 + @grade_2 + @grade_3 )*1.0/3
+   def average_248
+      (@grade_1_248 + @grade_2_248 + @grade_3_248 )*1.0/3
    end
 
    def showInformation
       super
-      puts "Code : #{code}\nAverage score: #{average};
+      puts "Code : #{code_248}\nAverage score: #{average_248};
        "
    end
 end
 
 # name, hair, age, height, weight, email, phone,nation, code, grade_1, grade_2, grade_3
-listStudent = Array.new
-student_1 = Student.new("Sy","Black",20,170, 70, "sybuivan1429@gmail.com","0947895039","VN",1911505310248, 8,9,7)
-student_2 = Student.new("Nhan","Black",20,170, 70, "Nhan@gmail.com","0947895039","VN",1911505310248, 8,9,6)
-student_3 = Student.new("Huy","Orange",20,170, 70, "Huy@gmail.com","0947895039","VN",1911505310248, 8,9,4)
-student_4 = Student.new("Thien","Black",20,170, 70, "Thien@gmail.com","0947895039","VN",1911505310248, 8,9,10)
+listStudent_248 = Array.new
+student_1_248 = Student.new("Sy","Black",20,170, 70, "sybuivan1429@gmail.com","0947895039","VN",1911505310248, 8,9,7)
+student_2_248 = Student.new("Nhan","Black",20,170, 70, "Nhan@gmail.com","0947895039","VN",1911505310248, 8,9,6)
+student_3_248 = Student.new("Huy","Orange",20,170, 70, "Huy@gmail.com","0947895039","VN",1911505310248, 8,9,4)
+student_4_248 = Student.new("Thien","Black",20,170, 70, "Thien@gmail.com","0947895039","VN",1911505310248, 8,9,10)
 
-listStudent.push(student_1, student_2,student_3,student_4)
+listStudent_248.push(student_1_248, student_2_248,student_3_248,student_4_248)
 puts '------------------Show student list :------------------'
-for i in 0..(listStudent.length-1)
+for i in 0..(listStudent_248.length-1)
     puts "------------Student #{i+1} ------------"
-    puts listStudent[i].showInformation
+    puts listStudent_248[i].showInformation
  end
 
  puts '------------------Sorted student list :------------------'
 
- studentSort = listStudent.sort_by{|student| [student.average]}.reverse
- for i in 0..(studentSort.length-1)
+ studentSort_248 = listStudent_248.sort_by{|student| [student.average_248]}.reverse
+ for i in 0..(studentSort_248.length-1)
     puts "------------Student #{i+1} ------------:"
-    puts studentSort[i].showInformation
+    puts studentSort_248[i].showInformation
  end
